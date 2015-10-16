d046a7b6b168cbfdf2dd5a3bdbb9d67738487ac5
Conor Maguire <conormag94@gmail.com>
2015-10-16 21:17:43 +0100

    Make get_standings take selected league parameter

--- main.py
+++ main.py
@@ -2,28 +2,44 @@ import json
 import requests
 from tabulate import tabulate
 
+import leagues
+
 BASE_URL = "http://api.football-data.org/alpha/"
 soccer_seasons = "soccerseasons/"
 
 epl_current_season = "soccerseasons/398/"
 league_table = "leagueTable/"
 
+# Gets current league table from selected league and calls print function
+def get_standings(league):
+	if league in leagues.LEAGUE_IDS:
+		request_url = "{}soccerseasons/{}/leagueTable".format(BASE_URL, leagues.LEAGUE_IDS[league])
+	else:
+		print "Error: No such league code"
+		
+	try:
+		resp = requests.get(request_url)
+		data = resp.json()	
+		print_standings(data['standing'])
+	except:
+		print "Error retrieving selected league table..."
 
+# Prints the league standings in a table
 def print_standings(table):
 	standings = []
 	for team in table:
-		entry = [team['position'], team['teamName'], team['points']]
+		entry = [
+			team['position'],
+			team['teamName'],
+			team['playedGames'],
+			team['points']
+		]
 		standings.append(entry)
 
-	print tabulate(standings, headers=['Pos', 'Club', 'Points'], tablefmt="rst")
+	print tabulate(standings, headers=['Pos', 'Club', 'Played', 'Points'], tablefmt="rst")
 
 def main():
-	resp = requests.get(BASE_URL + epl_current_season + league_table)
-	data = resp.json()
-
-	league_standings = data['standing']
-	
-	print_standings(league_standings)
+	get_standings("EP")
 
 if __name__ == '__main__':
 	main()

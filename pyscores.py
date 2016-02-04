c71897edd60b584d568bcb71c2c7c1ea62589077
Conor Maguire <conormag94@gmail.com>
2016-02-04 13:29:41 +0000

    Add days filter to specify number of days for which to show results/fixtures

--- pyscores.py
+++ pyscores.py
@@ -14,19 +14,6 @@ API_KEY = secret.secret_key
 
 headers = {'X-Auth-Token' : API_KEY}
 
-def get_fixtures(league, time_frame=40):
-	if league in config.LEAGUE_IDS:
-		request_url = "{}soccerseasons/{}/fixtures?timeFrame=n{}".format(BASE_URL, config.LEAGUE_IDS[league], time_frame)
-	else:
-		print("Error: No such league code")
-
-	try:
-		resp = requests.get(request_url, headers=headers)
-		data = resp.json()
-		print_fixtures(data['fixtures'])
-	except:
-		print("Error retrieving fixtures")
-
 def print_fixtures(array):
 	current_matchday = array[0]['matchday']
 	fixtures = []
@@ -41,23 +28,6 @@ def print_fixtures(array):
 			]
 			fixtures.append(s)
 	print(tabulate(fixtures, tablefmt="plain"))
-
-# Gets results for the most recent matchday
-def get_results(league, time_frame=7):
-	if league in config.LEAGUE_IDS:
-		request_url = "{}soccerseasons/{}/fixtures?timeFrame=p{}".format(BASE_URL, config.LEAGUE_IDS[league], time_frame)
-	else:
-		print("Error: No such league code")
-
-	try:
-		resp = requests.get(request_url, headers=headers)
-		data = resp.json()
-		if data['count'] == 0:
-			print("API returned 0 results for last {} days".format(time_frame))
-		else:
-			print_results(data['fixtures'])
-	except:
-		print("Error retrieving recent results")
 
 # Prints and colour codes recent results
 def print_results(array):
@@ -86,20 +56,6 @@ def print_results(array):
 			results.append(s)
 	print(tabulate(results, tablefmt="plain"))
 
-# Gets current league table from selected league and calls print function
-def get_standings(league):
-	if league in config.LEAGUE_IDS:
-		request_url = "{}soccerseasons/{}/leagueTable".format(BASE_URL, config.LEAGUE_IDS[league])
-	else:
-		print("Error: No such league code")
-
-	try:
-		resp = requests.get(request_url, headers=headers)
-		data = resp.json()
-		print_standings(data['standing'])
-	except:
-		print("Error retrieving selected league table...")
-
 # Prints the league standings in a table
 def print_standings(table):
 	standings = []
@@ -118,26 +74,74 @@ def print_standings(table):
 
 	print(tabulate(standings, headers=['#', 'Team', 'Games', 'W', 'D', 'L', 'GD', 'Pts'], tablefmt="simple"))
 
+def get_fixtures(league, time_frame=7):
+	if league in config.LEAGUE_IDS:
+		request_url = "{}soccerseasons/{}/fixtures?timeFrame=n{}".format(BASE_URL, config.LEAGUE_IDS[league], time_frame)
+	else:
+		print("Error: No such league code")
+
+	try:
+		resp = requests.get(request_url, headers=headers)
+		data = resp.json()
+		if data['count'] == 0:
+			print("API returned 0 fixtures within next {} days".format(time_frame))
+		else:
+			print_fixtures(data['fixtures'])
+	except:
+		print("Error retrieving fixtures")
+
+# Gets results for the most recent matchday
+def get_results(league, time_frame=7):
+	if league in config.LEAGUE_IDS:
+		request_url = "{}soccerseasons/{}/fixtures?timeFrame=p{}".format(BASE_URL, config.LEAGUE_IDS[league], time_frame)
+	else:
+		print("Error: No such league code")
+
+	try:
+		resp = requests.get(request_url, headers=headers)
+		data = resp.json()
+		if data['count'] == 0:
+			print("API returned 0 results within last {} days".format(time_frame))
+		else:
+			print_results(data['fixtures'])
+	except:
+		print("Error retrieving recent results")
+
+# Gets current league table from selected league and calls print function
+def get_standings(league):
+	if league in config.LEAGUE_IDS:
+		request_url = "{}soccerseasons/{}/leagueTable".format(BASE_URL, config.LEAGUE_IDS[league])
+	else:
+		print("Error: No such league code")
+
+	try:
+		resp = requests.get(request_url, headers=headers)
+		data = resp.json()
+		print_standings(data['standing'])
+	except:
+		print("Error retrieving selected league table...")
+
 def format_date(date_str):
 	months = {'01':'Jan', '02':'Feb', '03':'Mar', '04':'Apr', '05':'May', '06':'Jun', '07':'Jul', '08':'Aug', '09':'Sep', '10':'Oct', '11':'Nov', '12':'Dec'}
 	return (date_str[8:10] + " " + months[date_str[5:7]] + " " + date_str[:4])
 
 @click.command()
-@click.option('--standings', '-s', multiple=True, is_flag=True, help='Current league standings for a particular league')
+@click.option('--standings', '-s', multiple=False, is_flag=True, help='Current league standings for a particular league')
 @click.option('--results', '-r', is_flag=True, help='Most recent matchday results')
 @click.option('--fixtures', '-f', is_flag=True, help='Upcoming fixtures for next matchday')
-@click.option('--league', '-l', help='Specified league code to retrieve results for', type=click.Choice(config.LEAGUE_IDS.keys()))
-def main(standings, results, fixtures, league):
+@click.option('--league', '-l', help='Specific league code to retrieve results for', type=click.Choice(config.LEAGUE_IDS.keys()))
+@click.option('--days', '-d', default=7, help="Number of days for which to fetch results/fixtures (Default = 7)")
+def main(standings, results, fixtures, league, days):
 	try:
 		if league:
 			if standings:
 				get_standings(league)
 
 			if results:
-				get_results(league)
+				get_results(league, days)
 
 			if fixtures:
-				get_fixtures(league)
+				get_fixtures(league, days)
 		else:
 			print("Please specify a league")
 	except:

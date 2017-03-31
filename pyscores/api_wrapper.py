f395576c666510e467b859d7a0c7c7ab14e5e2e5
Conor Maguire <conormag94@gmail.com>
2017-04-01 00:23:39 +0100

    Add endpoints for the competition resource in api wrapper

--- pyscores/api_wrapper.py
+++ pyscores/api_wrapper.py
@@ -1,3 +1,5 @@
+"""A wrapper class for the football-data API"""
+
 import os
 
 import requests
@@ -27,10 +29,35 @@ class APIWrapper(object):
         return None
 
     def all_competitions(self):
-        url = "%s/competitions" % self.base_url
+        url = "{0}/competitions".format(self.base_url)
         response = self.do_request(url=url)
         return response
 
+    def competition(self, competition_id):
+        """Return a JSON object for a competition by its id."""
+        url = "{0}/competitions/{1}".format(self.base_url, competition_id)
+        response = self.do_request(url=url)
+        return response
+
+    #TODO: Add filters
+    def competition_teams(self, competition_id):
+        """Return a JSON list of teams in a particular competition."""
+        url = "{0}/competitions/{1}/teams".format(self.base_url, competition_id)
+        response = self.do_request(url=url)
+        return response
+
+    # TODO: Add filters
+    def competition_fixtures(self, competition_id):
+        """Returns a JSON list of all fixtures, past and present, in a competition"""
+        url = "{0}/competitions/{1}/fixtures".format(self.base_url, competition_id)
+        response = self.do_request(url=url)
+        return response
+
+    def competition_table(self, competition_id, filters=None):
+        """Return the current league table for a competition"""
+        url = "{0}/competitions/{1}/leagueTable".format(self.base_url, competition_id)
+        response = self.do_request(url=url, filters=filters)
+        return response
 
 def main():
     api = APIWrapper(auth_token=os.environ["PYSCORES_KEY"])
